802d0a9244a73b529fc8bca97386e10b4e236ce2
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-07-14 22:55:33 +0530

    Testing Controller Class Register Functionality

---
+++ src/test/java/com/user/controller/UserServiceControllerTest.java
@@ -0,0 +1,71 @@
+package com.user.controller;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.user.model.User;
+import com.user.service.UserService;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = UserServiceController.class)
+public class UserServiceControllerTest {
+
+	public static final String REGISTER_ENDPOINT = "/api/vi/users/register";
+	private static final String EMAILONE = "one@mail.com";
+	private User savedUserOne;
+	private User userOne;
+	private User userOneInvalid;
+
+	@MockBean
+	private UserService service;
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Autowired
+	private ObjectMapper mapper; // Converts String to Json and Vice versa. Present in Jackson
+
+	@BeforeEach
+	public void setUp() {
+
+		userOne = new User(0, EMAILONE, "one", null);
+		savedUserOne = new User(1, EMAILONE, "one", null);
+		userOneInvalid = new User(0, EMAILONE, "one@", null);
+	}
+
+	@Test
+	public void givenUserDetailsWhenUserDoesNotExistThenRegistrationSuccessful() throws Exception {
+
+		// Setup the mock bean behaviour
+		when(service.registerUser(any(User.class))).thenReturn(savedUserOne);
+
+		System.out.println(mapper.writeValueAsString(userOne));
+		MvcResult mvcResult = mvc
+						.perform(post(REGISTER_ENDPOINT)
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(mapper.writeValueAsString(userOne))) /* Conversion of the userOne Object into JSON */
+						.andExpect(status().isOk()).andDo(MockMvcResultHandlers.print()).andReturn();
+
+		assertTrue(mvcResult.getResponse().getContentAsString().equals("User Registered :1"));
+		
+		verify(service).registerUser(any(User.class));
+	}
+
+}
